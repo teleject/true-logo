ea4327697f2ddeffa28e9d3b6c54c3b64d7b7a93
teleject <schmitt@christopher.org>
2019-03-06 16:17:27 -0600

    Adding more sites

--- background.js
+++ background.js
@@ -1,20 +1,18 @@
-(function () {
-
+(function() {
   "use strict";
 
   var off,
+    loadJS = function() {
+      chrome.tabs.executeScript(null, {
+        file: off ? "off.js" : "on.js"
+      });
+    };
 
-  loadJS = function(){
-    chrome.tabs.executeScript(null, {
-      file: off ? "off.js" : "on.js"
-    });
-  };
-
-  chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.browserAction.getBadgeText({}, function(text){
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.browserAction.getBadgeText({}, function(text) {
       off = text == "off";
       chrome.browserAction.setBadgeText({
-        text: off ? '' : 'off'
+        text: off ? "" : "off"
       });
       off = !off;
       loadJS();
@@ -22,12 +20,11 @@
   });
 
   chrome.tabs.onUpdated.addListener(function(tab, response) {
-    if (response.status === 'complete') {
-      chrome.browserAction.getBadgeText({},function(text){
+    if (response.status === "complete") {
+      chrome.browserAction.getBadgeText({}, function(text) {
         off = text == "off";
         loadJS();
       });
     }
   });
-
-}());
+})();
